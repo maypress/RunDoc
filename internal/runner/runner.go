3b57c893c92e2324a2ab19f00d714dd8667996ef
maypress <kernelzcode@gmail.com>
2026-03-28 21:38:50 +0300

    feat: adding package and main_function

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -1,3 +1,9 @@
 // Интерфейс для выполнения кода на разных языках.
 // Определяет Runner interface и структуру Result.
 // GetRunner возвращает конкретную реализацию для указанного языка.
+
+package runner
+
+func runner() {
+	// runner func
+}
