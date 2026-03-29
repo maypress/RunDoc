85084137ca6543d6f9d192bd7f46f2cf72623a8e
maypress <kernelzcode@gmail.com>
2026-03-29 12:41:59 +0300

    feat: extentions-runner

---
+++ internal/runner/extension/bash.go
@@ -0,0 +1,38 @@
+// bash runner
+
+package extensions
+
+import (
+	"bytes"
+	"os/exec"
+	"strings"
+
+	"github.com/maypress/RunDoc/internal/runner"
+)
+
+type BashRunner struct{}
+
+func (r BashRunner) Run(code []string) runner.Result {
+	cmd := exec.Command("bash", "-c", strings.Join(code, "\n"))
+
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	exitCode := 0
+	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			exitCode = exitErr.ExitCode()
+		} else {
+			return runner.Result{Error: err, ExitCode: -1}
+		}
+	}
+
+	output := stdout.String()
+	if stderr.Len() > 0 {
+		output += stderr.String()
+	}
+
+	return runner.Result{Output: output, ExitCode: exitCode}
+}
